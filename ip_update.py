9bb07e41b9540532d4c81edb5136ab84cfd58e69
bengarlock <ben.garlock@gmail.com>
2021-08-06 07:45:28 -0400

    cleared up pycharm issues

--- ip_update.py
+++ ip_update.py
@@ -8,9 +8,11 @@ my_ip = str(my_ip.text)
 
 url = f'https://{return_username(1)}:{return_password(1)}@domains.google.com/nic/update?hostname=bengarlock.com&myip={my_ip}'
 update_ip_response1 = requests.post(url)
+print(update_ip_response1.text)
 
 url = f'https://{return_username(2)}:{return_password(2)}@domains.google.com/nic/update?hostname=www.bengarlock.com&myip={my_ip}'
 update_ip_response2 = requests.post(url)
+print(update_ip_response2.text)
 
 with open(os.path.join(os.getcwd(), 'ip_log.txt'), 'a', encoding='utf-8') as file:
     file.write(f'{datetime.now()}       bengarlock.com          {update_ip_response1.text} \n')
